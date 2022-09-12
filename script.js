97f69ea5675bf31bb2c3d180bb4244061f776330
hroist <111881583+hroist@users.noreply.github.com>
2022-09-12 11:53:33 +0200

    shop by category section added. Cart badge added.

--- script.js
+++
@@ -1,8 +0,0 @@
-
-var parent = document.querySelector('.info-box')
-parent.addEventListener('mouseover', function(){
-  parent.children[0].style.backgroundColor = 'black';
-})
-parent.addEventListener('mouseout', function(){
-  parent.children[0].style.backgroundColor = 'white';
-})
